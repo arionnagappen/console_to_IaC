0c97db867f18d3de8de18af83a014c65577c52e7
Arion <arionnagappen@gmail.com>
2025-06-10 21:01:02 +0200

    Vpc Infrastructure

---
+++ cdk-app/lib/constructs/networking/my- vpc-construct.ts
@@ -0,0 +1,37 @@
+import { Construct } from 'constructs';
+import * as ec2 from 'aws-cdk-lib/aws-ec2';
+
+export interface MyVpcProps {
+  cidr: string;
+  maxAzs?: number;
+}
+
+export class MyVPC extends Construct {
+  public readonly vpc: ec2.Vpc;
+
+  constructor(scope: Construct, id: string, props: MyVpcProps){
+    super(scope, id)
+
+    this.vpc = new ec2.Vpc(this, 'MyVPC', {
+      ipAddresses: ec2.IpAddresses.cidr(props.cidr),
+
+      maxAzs: props.maxAzs ?? 2,
+
+      subnetConfiguration: [
+        {
+          name: 'application-subnet',
+          subnetType: ec2.SubnetType.PUBLIC,
+          cidrMask: 24,
+          mapPublicIpOnLaunch: true
+        },
+
+        {
+          name: 'database-subnet',
+          subnetType: ec2.SubnetType.PRIVATE_ISOLATED,
+          cidrMask: 24
+        }
+      ]
+    })
+  }
+
+}
