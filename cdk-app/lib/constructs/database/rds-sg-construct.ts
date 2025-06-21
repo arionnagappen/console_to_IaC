d80278c48da799e16ab8e6f4103eaad24a6dbef9
Arion <arionnagappen@gmail.com>
2025-06-21 19:18:58 +0200

    database construct & stack

---
+++ cdk-app/lib/constructs/database/rds-sg-construct.ts
@@ -0,0 +1,32 @@
+import { Construct } from "constructs";
+import * as ec2 from 'aws-cdk-lib/aws-ec2';
+
+export interface DBSgProps {
+  vpc: ec2.IVpc;
+  appSecurityGroup: ec2.ISecurityGroup
+}
+
+export class DatabaseSecurityGroup extends Construct {
+  public readonly dbSecurityGroup: ec2.SecurityGroup;
+
+  constructor(scope: Construct, id: string, props: DBSgProps){
+    super(scope, id)
+
+    this.dbSecurityGroup = new ec2.SecurityGroup(this, 'DBSecurityGroup', {
+      vpc: props.vpc,
+      description: 'RDS Database Security Group',
+    })
+
+    // Only allow inbound traffic on port 3306
+    this.dbSecurityGroup.addIngressRule(
+      ec2.Peer.securityGroupId(props.appSecurityGroup.securityGroupId),
+      ec2.Port.tcp(3306)
+    )
+
+    // Only allow outbound traffic on port 3306
+    this.dbSecurityGroup.addEgressRule(
+      ec2.Peer.anyIpv4(),
+      ec2.Port.tcp(3306)
+    )
+  }
+}
